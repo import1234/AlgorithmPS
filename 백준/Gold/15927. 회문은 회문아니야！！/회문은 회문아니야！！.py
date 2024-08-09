bbd860e64e83b3cae01a1803ff232e783a093a58
import1234 <162861691+import1234@users.noreply.github.com>
2024-08-09 16:38:35 +0900

    [Gold V] Title: 회문은 회문아니야!!, Time: 52 ms, Memory: 35516 KB -BaekjoonHub

--- 백준/Gold/15927. 회문은 회문아니야！！/회문은 회문아니야！！.py
+++ 백준/Gold/15927. 회문은 회문아니야！！/회문은 회문아니야！！.py
@@ -2,6 +2,4 @@ c=lambda x:x!=x[::-1]
 s=input()
 if len(set(list(s)))==1:print(-1);exit()
 if c(s):print(len(s))
-else:
-    if c(s[:-1]) or c(s[1:]):print(len(s)-1)
-    else:print(len(s)-2)
+else:print(len(s)-1)
