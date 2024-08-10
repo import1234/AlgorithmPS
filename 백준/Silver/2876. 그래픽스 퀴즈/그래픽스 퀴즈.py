74b6319f2565f68add6711f736ed1f2081f99166
import1234 <162861691+import1234@users.noreply.github.com>
2024-08-11 08:17:22 +0900

    [Silver III] Title: 그래픽스 퀴즈, Time: 168 ms, Memory: 125720 KB -BaekjoonHub

--- 백준/Silver/2876. 그래픽스 퀴즈/그래픽스 퀴즈.py
+++ 백준/Silver/2876. 그래픽스 퀴즈/그래픽스 퀴즈.py
@@ -1,10 +1,16 @@
-l=[[0]*6]
-for x in '0'*int(input()):
-    t=[0]*6
-    for x in map(int,input().split()):t[x]=l[-1][x]+1
+n=int(input())
+l=[[0,0,0,0,0]]
+for x in range(n):
+    a,b=map(int,input().split())
+    t=[0,0,0,0,0]
+    for y in[a,b]:t[y-1]=l[-1][y-1]+1
     l.append(t)
-M=i=0
+M=0
+i=100
 for x in l:
-    if M<max(x):M=max(x);i=x.index(M)
-    elif M==max(x) and x.index(M)<i:i=x.index(M)
+    if M<max(x):
+        M=max(x)
+        i=x.index(M)+1
+    elif M==max(x):
+        if x.index(M)+1<i:i=x.index(M)+1
 print(M,i)
