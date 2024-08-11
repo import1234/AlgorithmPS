93f77ecfcb66d0a83a62ec90307607c04818ee3d
import1234 <162861691+import1234@users.noreply.github.com>
2024-08-11 19:34:24 +0900

    [Silver I] Title: 팩토리얼 0의 개수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11687. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
+++ 백준/Silver/11687. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -2,11 +2,9 @@ n=int(input())
 l=1
 h=10**8
 while l<=h:
-    m=(l+h)//2
-    s=0
-    for x in range(1,20):s+=(5*m)//5**x
-    #print(l,h,5*m,s)
-    if s>n:h=m-1
-    elif s<n:l=m+1
-    else:print(5*m);exit()
+ m=(l+h)//2
+ s=sum((5*m)//5**x for x in range(1,20))
+ if s>n:h=m-1
+ elif s<n:l=m+1
+ else:print(5*m);exit()
 print(-1)
