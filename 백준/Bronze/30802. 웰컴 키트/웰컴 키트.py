02a8b397caf59dbc598ad68b0e1391e3615e607d
import1234 <162861691+import1234@users.noreply.github.com>
2024-08-12 06:10:21 +0900

    [Bronze III] Title: 웰컴 키트, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
@@ -1,5 +1,6 @@
-n=int(input())
-l=[*map(int,input().split())]
-t,p=map(int,input().split())
+f=lambda:map(int,input().split())
+n=sum(f())
+l=[*f()]
+t,p=f()
 print(sum(1+(x-1)//t for x in l))
 print(n//p,n%p)
