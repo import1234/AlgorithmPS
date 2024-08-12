6c75b38caa6f971661ef0d581e91d111405a9502
import1234 <162861691+import1234@users.noreply.github.com>
2024-08-13 08:43:10 +0900

    [Gold I] Title: 기벡을 안배운다고?, Time: 500 ms, Memory: 151008 KB -BaekjoonHub

--- 백준/Gold/28132. 기벡을 안배운다고？/기벡을 안배운다고？.py
+++ 백준/Gold/28132. 기벡을 안배운다고？/기벡을 안배운다고？.py
@@ -1,21 +1,14 @@
-import math,sys
-i=sys.stdin.readline
-
-n=int(i())
-l={}
-for _ in range(n):
-    a,b=map(int,i().split())
-    g=math.gcd(a,b)
-    if g==0:g=1
-    a//=g;b//=g
-    if l.get((a,b)):l[(a,b)]+=1
-    else:l[(a,b)]=1
-
-r=0
+import math
+n,r,l=int(input()),0,{}
+for _ in '1'*n:
+ a,b=map(int,input().split());g=math.gcd(a,b)
+ if g==0:g=1
+ a//=g;b//=g;l[(a,b)]=1+l[(a,b)]if l.get((a,b))else 1
 for a,b in l:
-    if a==0 and b==0:r+=l[(0,0)]**2-l[(0,0)]
-    else:
-        if l.get((b,-a)):r+=l[(a,b)]*l[(b,-a)]
-        if l.get((-b,a)):r+=l[(a,b)]*l[(-b,a)]
-        if l.get((0,0)):r+=2*l[(a,b)]*l[(0,0)]
+ t=l[(a,b)]
+ if a==0 and b==0:r+=t*t-t
+ else:
+  if l.get((b,-a)):r+=t*l[(b,-a)]
+  if l.get((-b,a)):r+=t*l[(-b,a)]
+  if l.get((0,0)):r+=2*t*l[(0,0)]
 print(r//2)
