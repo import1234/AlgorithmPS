0b3b105de4dea444cad3030fb995af408a433fbd
import1234 <162861691+import1234@users.noreply.github.com>
2024-08-12 20:59:52 +0900

    [Silver II] Title: 회의실 배정 3, Time: 168 ms, Memory: 121300 KB -BaekjoonHub

--- 백준/Silver/19622. 회의실 배정 3/회의실 배정 3.py
+++ 백준/Silver/19622. 회의실 배정 3/회의실 배정 3.py
@@ -1,8 +1,5 @@
 n=int(input())
-l=[]
-for _ in range(n):l.append([*map(int,input().split())])
-l.sort(key=lambda x:x[0])
-if n<3:print(max(l[0][2],l[1*(n-1)][2]));exit()
-l[2][2]+=l[0][2]
-for x in range(3,n):l[x][2]+=max(l[x-2][2],l[x-3][2])
-print(max(l[-x][2]for x in[1,2,3]))
+l,d=[],[0,0]
+for x in range(n):l.append([*map(int,input().split())][2])
+for x in range(n):d.append(max(d[x]+l[x],d[x+1]))
+print(d[-1])
