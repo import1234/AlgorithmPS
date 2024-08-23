0fa23e36dd06ffd79b72ca5b958478079f0a2797
import1234 <162861691+import1234@users.noreply.github.com>
2024-08-23 21:04:47 +0900

    [Gold IV] Title: 3루수는 몰라, Time: 252 ms, Memory: 135644 KB -BaekjoonHub

--- 백준/Gold/17351. 3루수는 몰라/3루수는 몰라.py
+++ 백준/Gold/17351. 3루수는 몰라/3루수는 몰라.py
@@ -1,23 +1,15 @@
 n=int(input())
-l=[]
-for x in range(n):
-    l.append([*input()])
-
-t={(0,0)}
+l,t=[],{(0,0)}
+for x in'0'*n:l+=[[*input()]]
 while q:=t:
     t=set()
     for a,b in q:
-        u=0
-        u=max(u,l[a-1][b]//4*4 if a>0 else 0,l[a][b-1]//4*4 if b>0 else 0)
-        if l[a][b]not in list('MOLA'):l[a][b]=u
-        else:
-            p=l[a][b]
-            l[a][b]=u+(l[a][b]=='M')
-            if a-1>=0 and l[a-1][b]%4=='MOLA'.find(p):l[a][b]=max(l[a][b],l[a-1][b]+1)
-            if b-1>=0 and l[a][b-1]%4=='MOLA'.find(p):l[a][b]=max(l[a][b],l[a][b-1]+1)
-        
+        u=max(l[a-1][b]//4*4if a else 0,l[a][b-1]//4*4if b else 0)
+        if l[a][b]in'MOLA':
+            p=l[a][b];l[a][b]=u+(l[a][b]=='M')
+            for i in[0,1]:
+                if[a,b][i]>0and l[a-~i%2][b-i]%4=='MOLA'.find(p):l[a][b]=max(l[a][b],l[a-~i%2][b-i]+1)
+        else:l[a][b]=u
         if a+1<n:t.add((a+1,b))
         if b+1<n:t.add((a,b+1))
-
-#for x in l:print(*x)
 print(l[-1][-1]//4)
