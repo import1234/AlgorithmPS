02cb91e13fb33310168281e74e844807536868d9
import1234 <162861691+import1234@users.noreply.github.com>
2024-08-27 14:35:49 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 116 ms, Memory: 113720 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -9,7 +9,7 @@ while l[n]==inf:
     t=set()
     for x in q:
         if x%2==0:t.add(x//2);l[x//2]=l[x]if l[x]<l[x//2]else l[x//2]
-        if x+1<=2*k and l[x+1]==inf:t.add(x+1);l[x+1]=1+l[x]if l[x]+1<l[x+1]else l[x+1]
+        if x+1<2*k and l[x+1]==inf:t.add(x+1);l[x+1]=1+l[x]if l[x]+1<l[x+1]else l[x+1]
         if x>0 and l[x-1]==inf:t.add(x-1);l[x-1]=1+l[x]if l[x]+1<l[x-1]else l[x-1]
     q=t
 print(l[n])
