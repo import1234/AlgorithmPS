f6920ac734cde909aac4c20ffb4bd9efa1f95989
import1234 <162861691+import1234@users.noreply.github.com>
2024-09-11 16:03:35 +0900

    [Gold IV] Title: 최단경로, Time: 672 ms, Memory: 151616 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,36 +1,32 @@
-import sys
+import sys,heapq
+from collections import defaultdict
 i=sys.stdin.readline
 
-#정점 리스트
-d={}
-V,E=map(int,i().split())
-k=int(i())
-for x in'0'*E:
+d=defaultdict(dict)
+n,e=map(int,i().split())
+k=int(i()) #시작점
+for x in'0'*e:
     u,v,w=map(int,i().split())
-    if u not in d:d[u]={}
-    if v in d[u]:d[u][v]=min(d[u][v],w)
-    else:d[u][v]=w
+    d[u][v]=min(d[u].get(v,float('inf')),w)
 
-#데이크스트라
-r={k:0} #최단거리가 확정된 점
-s={} #최단거리가 확정 안된 점(inf는 생략)
-#k 최단거리가 가장 최근에 확정된 점
+V={k:0} #최단거리가 확정된 노드
+S={} #최단거리가 확정 안된 노드(inf는 생략)
+q=[] #최소 힙
+#k 최단거리가 가장 최근에 확정된 노드
 while 1:
-    if k in d:
-        for x in d[k]:
-            if x in r:continue #이미 최단거리가 확정된 점이면
-            if x in s:s[x]=min(s[x],r[k]+d[k][x])
-            else:s[x]=r[k]+d[k][x]
-    if s=={}:break
+    for x in d[k]:
+        if x in V:continue #이미 최단거리가 확정된 노드면
+        S[x]=min(S.get(x,float('inf')),V[k]+d[k][x])
+        heapq.heappush(q,(S[x],x))
+    if S=={}:break #최단거리가 확정되지 않은 노드가 없으면 종료
+
     m=float('inf')
-    for x in s:
-        if s[x]<m:
-            m=s[x]
-            k=x
-    r[k]=m
-    del s[k]
+    while 1:
+        m,k=heapq.heappop(q)
+        if k not in V:break
+    
+    V[k]=m #해당 노드 V로 확정
+    del S[k] #S에서는 제거
 
 #출력
-for x in range(1,V+1):
-    if x in r:print(r[x])
-    else:print('INF')
+for x in range(1,n+1):print(V[x] if x in V else 'INF')
