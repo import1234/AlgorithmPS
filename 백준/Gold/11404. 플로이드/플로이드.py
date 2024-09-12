21187488026134da70c79530bcab7cd7f8ffa77d
import1234 <162861691+import1234@users.noreply.github.com>
2024-09-12 11:19:34 +0900

    [Gold IV] Title: 플로이드, Time: 180 ms, Memory: 111540 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,29 +1,19 @@
-from collections import defaultdict
-from sys import stdin
+INF=float('inf')
 n,m=int(input()),int(input())
-d=defaultdict(dict)
-for x in range(m):
-    a,b,c=map(int,stdin.readline().split())
-    a-=1;b-=1
-    if b in d[a]:d[a][b]=min(d[a][b],c)
-    else:d[a][b]=c
 
 #거리 초기화
-l=[]
-for x in range(n):
-    l.append([])
-    for y in range(n):
-        if x==y:l[x].append(0)
-        else:l[x].append(d[x][y] if y in d[x] else float('inf'))
+l=[[INF]*n for _ in range(n)]
+for x in range(m):
+    a,b,c=map(int,input().split())
+    a-=1;b-=1
+    l[a][b]=min(l[a][b],c)
 
 for x in range(n):
+    l[x][x]=0
     for y in range(n):
         for z in range(n):
             l[y][z]=min(l[y][z],l[y][x]+l[x][z])
 
 for x in l:
-    s=''
     for y in x:
-        if y==float('inf'):s+='0 '
-        else:s+=str(y)+' '
-    print(s)
+        print(0 if y==INF else y)
