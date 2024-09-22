1f1537fe74a4db21ea0204a8a067c8b8fd21e82c
import1234 <162861691+import1234@users.noreply.github.com>
2024-09-22 15:04:43 +0900

    [Gold V] Title: 초코칩 케이크, Time: 164 ms, Memory: 118636 KB -BaekjoonHub

--- 백준/Gold/23823. 초코칩 케이크/초코칩 케이크.py
+++ 백준/Gold/23823. 초코칩 케이크/초코칩 케이크.py
@@ -11,40 +11,14 @@ for x in' '*q:
     t,a=map(int,i().split())
     if t==1:
         dx[a]=dx.get(a,0)+1
-        if dy=={}:
-            ans=0
-            if dx[a]>layerX:
-                layerX=dx[a]
-                layerXlist=[a]
-            elif dx[a]==layerX:
-                layerXlist.append(a)
-            ans=len(layerXlist)*n
-        else:
-            ans=0
-            if dx[a]>layerX:
-                layerX=dx[a]
-                layerXlist=[a]
-            elif dx[a]==layerX:
-                layerXlist.append(a)
-            ans=len(layerXlist)*len(layerYlist)
-
+        ans=0
+        if dx[a]>layerX:layerX=dx[a];layerXlist=[a]
+        elif dx[a]==layerX:layerXlist.append(a)
+        ans=len(layerXlist)*(len(layerYlist)if dy else n)
     else:
         dy[a]=dy.get(a,0)+1
-        if dx=={}:
-            ans=0
-            if dy[a]>layerY:
-                layerY=dy[a]
-                layerYlist=[a]
-            elif dy[a]==layerY:
-                layerYlist.append(a)
-            ans=n*len(layerYlist)
-        else:
-            ans=0
-            if dy[a]>layerY:
-                layerY=dy[a]
-                layerYlist=[a]
-            elif dy[a]==layerY:
-                layerYlist.append(a)
-            ans=len(layerXlist)*len(layerYlist)
-
+        ans=0
+        if dy[a]>layerY:layerY=dy[a];layerYlist=[a]
+        elif dy[a]==layerY:layerYlist.append(a)
+        ans=(len(layerXlist)if dx else n)*len(layerYlist)
     print(ans)
