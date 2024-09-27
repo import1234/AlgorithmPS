495b22f30672598cf19164d6ff460c0cb630e22f
import1234 <162861691+import1234@users.noreply.github.com>
2024-09-27 13:18:17 +0900

    [Gold III] Title: 움직이는 미로 탈출, Time: 92 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.py
+++ 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.py
@@ -1,16 +1,13 @@
-l=[[*input()]for x in range(8)]
-
+l=open(0).read().split()
 t={(7,0)}
 while q:=t:
-    t=set()
-    for x,y in q:
-        if l[x][y]=='#':continue
-        for a,b in[[0,0],[0,1],[-1,0],[0,-1],[0,-1],[1,0],[1,0],[0,1],[0,1]]:
-            x+=a;y+=b
-            if 0<=x<8 and 0<=y<8 and l[x][y]!='#':
-                if (x,y)==(0,7):print(1);exit()
-                t.add((x,y))
-    l.pop()
-    l.insert(0,[0]*8)
-
+ t=set()
+ for x,y in q:
+  if l[x][y]=='#':continue
+  for a,b in[[0,0],[0,1],[-1,0],[0,-1],[0,-1],[1,0],[1,0],[0,1],[0,1]]:
+   x+=a;y+=b
+   if 0<=x<8and 0<=y<8and l[x][y]!='#':
+    if(x,y)==(0,7):print(1);exit()
+    t.add((x,y))
+ l.insert(0,'0'*8)
 print(0)
