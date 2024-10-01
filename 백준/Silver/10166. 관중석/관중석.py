abc2006b7477865fc763e7559aaf8dc770306fe8
import1234 <162861691+import1234@users.noreply.github.com>
2024-10-01 18:31:21 +0900

    [Silver I] Title: 관중석, Time: 692 ms, Memory: 33240 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10166. 관중석/관중석.py
+++ 백준/Silver/10166. 관중석/관중석.py
@@ -1,6 +1,13 @@
 from math import gcd
 a,b=map(int,input().split())
 l=set()
+count=0
 for x in range(a,b+1):
-    for y in range(x):g=gcd(x,y);l.add(f'{y//g}/{x//g}')
-print(len(l))
+    t=set()
+    for y in range(x):
+        g=gcd(x,y)
+        if x//g not in l:
+            t.add(x//g)
+            count+=1
+    l.update(t)
+print(count)
