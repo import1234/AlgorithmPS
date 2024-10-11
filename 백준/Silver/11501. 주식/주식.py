da1248fd768f737cf78088cb81ff6c3a6eee606a
import1234 <162861691+import1234@users.noreply.github.com>
2024-10-11 23:38:06 +0900

    [Silver II] Title: 주식, Time: 1224 ms, Memory: 336196 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.py
+++ 백준/Silver/11501. 주식/주식.py
@@ -1,12 +1,8 @@
 for x in range(int(input())):
-    n=int(input())
-    l=[*zip(map(int,input().split()),range(n))]
-    s=sorted(l,reverse=1)
-    ans=0
-    p=0
-    for a,b in s:
-        if b<p:continue
-        for y in range(p,b):
-            ans+=a-l[y][0]
-        p=b+1
+    input()
+    l=[*map(int,input().split())]
+    c=ans=0
+    for x in l[::-1]:
+        if x>c:c=x
+        else:ans+=c-x
     print(ans)
