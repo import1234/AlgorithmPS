0985a12f69d67f1d36db44fc5f79e7fcd05a1db9
import1234 <162861691+import1234@users.noreply.github.com>
2024-10-19 04:38:27 +0900

    [Silver I] Title: 뒤풀이, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/14575. 뒤풀이/뒤풀이.py
+++ 백준/Silver/14575. 뒤풀이/뒤풀이.py
@@ -1,28 +1,8 @@
-n,t=map(int,input().split())
-li=[]
-a=b=sumL=sumR=0
-for x in range(n):
-    l,r=map(int,input().split())
-    li.append(r)
-    sumL+=l
-    sumR+=r
-    a=max(l,a)
-    b=max(r,b)
-
-if not sumL<=t<=sumR:
-    print(-1)
-    exit()
-
-def f(m):
-    s=0
-    for y in li:
-        s+=min(m,y)
-        if s>=t:return 1
-    return 0
-
+n,t,*l=map(int,open(0).read().split())
+a,b=max(l[::2]),max(l[1::2])
+if not sum(l[::2])<=t<=sum(l[1::2]):print(-1);exit()
 while a<b:
     m=(a+b)//2
-    if f(m):b=m
+    if sum(min(m,x)for x in l[1::2])>=t:b=m
     else:a=m+1
-
 print(a)
