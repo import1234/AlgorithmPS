0db892b849de3d5467f64cb7fbd77dd344c8c84a
import1234 <162861691+import1234@users.noreply.github.com>
2024-11-01 22:32:13 +0900

    [Silver III] Title: 자전거 묘기, Time: 220 ms, Memory: 54036 KB -BaekjoonHub

--- 백준/Silver/25706. 자전거 묘기/자전거 묘기.py
+++ 백준/Silver/25706. 자전거 묘기/자전거 묘기.py
@@ -1,5 +1,4 @@
-n=int(input())
-l=[*map(int,input().split())]
+n,*l=map(int,open(0).read().split())
 dp=[0]*n
-for x in range(n):dp[n-x-1]=dp[n-x+l[n-x-1]]+1 if n-x+l[n-x-1]<n else 1
+for x in range(n):dp[n-x-1]=dp[n-x+l[n-x-1]]+1 if l[n-x-1]<x else 1
 print(*dp)
