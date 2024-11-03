80e1bcc858fdaed85da225782fc131741e75bf8a
import1234 <162861691+import1234@users.noreply.github.com>
2024-11-03 19:44:11 +0900

    [Silver I] Title: 물정수열, Time: 336 ms, Memory: 103048 KB -BaekjoonHub

--- 백준/Silver/26648. 물정수열/물정수열.py
+++ 백준/Silver/26648. 물정수열/물정수열.py
@@ -1,10 +1,5 @@
-n=int(input())
-A=[*map(int,input().split())]
-B=[*map(int,input().split())]
-C=[*map(int,input().split())]
-dp=[-1]*(n+1)
+n,*l=map(int,open(0).read().split())
+p=-1
 for x in range(n):
-    a,b,c=sorted([A[x],B[x],C[x]])
-    dp[x+1]=max(dp[x]+1,a)
-    if dp[x+1]>c or dp[x+1]==-1:print('NO');exit()
+ if(p:=max(p+1,min(t:=l[x::n])))>max(t):print('NO');exit()
 print('YES')
