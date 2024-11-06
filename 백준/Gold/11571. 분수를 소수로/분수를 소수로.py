dafc23119ad456afc429168fe8f481c960de11ff
import1234 <162861691+import1234@users.noreply.github.com>
2024-11-06 20:34:32 +0900

    [Gold V] Title: 분수를 소수로, Time: 968 ms, Memory: 131504 KB -BaekjoonHub

--- 백준/Gold/11571. 분수를 소수로/분수를 소수로.py
+++ 백준/Gold/11571. 분수를 소수로/분수를 소수로.py
@@ -1,15 +1,10 @@
 for _ in ' '*int(input()):
-    d={}
     a,b=map(int,input().split())
-    r=[f'{a//b}.']
-    a%=b
-    i=1
-    while 1:
-        a*=10
-        if (a,a//b) in d:break
-        d[(a,a//b)]=i
+    r=[f'{a//b}.'];i=0;d={}
+    while i:=i+1:
+        a%=b;a*=10
+        if(a,b)in d:break
+        d[(a,b)]=i
         r.append(f'{a//b}')
-        a%=b
-        i+=1
-    r.insert(d[(a,a//b)],'(')
+    r.insert(d[(a,b)],'(')
     print(''.join(r)+')')
