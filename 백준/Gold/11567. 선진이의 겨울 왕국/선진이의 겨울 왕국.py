b7f64aa4888fed23bb854037eb4848193028b2eb
import1234 <162861691+import1234@users.noreply.github.com>
2024-11-11 22:44:20 +0900

    [Gold III] Title: 선진이의 겨울 왕국, Time: 124 ms, Memory: 120608 KB -BaekjoonHub

--- 백준/Gold/11567. 선진이의 겨울 왕국/선진이의 겨울 왕국.py
+++ 백준/Gold/11567. 선진이의 겨울 왕국/선진이의 겨울 왕국.py
@@ -1,18 +1,22 @@
-n,m=map(int,input().split())
-l=[['X']*(m+2)]+[list('X'+input()+'X')for x in range(n)]+[['X']*(m+2)]
-r1,c1=map(int,input().split())
-r2,c2=map(int,input().split())
+def f(x):print(x);exit()
+g=lambda:map(int,input().split())
+
+n,m=g()
+l=['X'*(m+2)]
+l+=[[*'X'+input()+'X']for x in' '*n]+l
+r1,c1=g();r2,c2=g()
+
 t={(r1,c1,0)}
 while q:=t:
     t=set()
     for x,y,z in q:
-        for a,b in [(0,1),(0,-2),(1,1),(-2,0)]:
+        for a,b in[(0,1),(0,-2),(1,1),(-2,0)]:
             x+=a;y+=b
             if l[x][y]!='X':
                 t.add((x,y,z))
                 l[x][y]='X'
             elif (x,y)==(r2,c2):
-                if l[x][y]=='X':print('YES');exit()
-                if z:print('YES');exit()
+                if l[x][y]=='X':f('YES')
+                if z:f('YES')
                 else:t.add((x,y,1))
-print('NO')
+f('NO')
