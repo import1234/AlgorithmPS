df4ae9668698e918179c1b7aec7556e949ccdc4b
import1234 <162861691+import1234@users.noreply.github.com>
2024-11-18 01:32:53 +0900

    [Silver I] Title: 아름다운 수열, Time: 172 ms, Memory: 39640 KB, Score: 150 point -BaekjoonHub

--- 백준/Silver/21605. 아름다운 수열/아름다운 수열.py
+++ 백준/Silver/21605. 아름다운 수열/아름다운 수열.py
@@ -1,6 +1,12 @@
-dp=[[0],[-1,1],[1,-1,-1,1],[1,1,-1,-1,-1,1],[1,1,-1,-1,1,-1,-1,1],[1,1,-1,-1,1,-1,1,-1,-1,1]]
-
+from collections import deque
 n=int(input())
-for x in range(6,n+1):
-    dp.append([1,1]+dp[x-3]+[-1,-1,-1,1])
-print(*dp[n])
+if n==1:print(-1,1)
+elif n==2:print(1,-1,-1,1)
+else:
+    dp=[deque([1,1,-1,-1,-1,1]),deque([1,1,-1,-1,1,-1,-1,1]),deque([1,1,-1,-1,1,-1,1,-1,-1,1])]
+    if n<=5:print(*dp[n-3]);exit()
+    for x in range(n-5):
+        dp[x%3].appendleft(1)
+        dp[x%3].appendleft(1)
+        for y in [-1,-1,-1,1]:dp[x%3].append(y)
+    print(*dp[x%3])
