58370763732e7e2f8cdf0cab3966a1bc15103c80
import1234 <162861691+import1234@users.noreply.github.com>
2024-11-22 00:47:52 +0900

    [Silver II] Title: 파닭파닭, Time: 1460 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Silver/14627. 파닭파닭/파닭파닭.py
+++ 백준/Silver/14627. 파닭파닭/파닭파닭.py
@@ -1,15 +1,11 @@
-import sys
-s,c=map(int,input().split())
-l=[int(sys.stdin.readline()) for x in range(s)]
+S,C,*l=map(int,open(0).read().split())
 
-low=0
-high=1000000000
-while low<=high:
-    mid=(low+high)//2
-    if mid==0:break
-    if sum(x//mid for x in l)>=c:
-        low=mid+1
-    else:
-        high=mid-1
+s=0
+e=10**9
+while s<=e:
+    m=(s+e)//2
+    if m==0:break
+    if sum(x//m for x in l)>=C:s=m+1
+    else:e=m-1
 
-print(sum(l)-high*c)
+print(sum(l)-e*C)
