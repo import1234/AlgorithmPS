910aaa0973444b854b9801cba9067314bc73ebf9
import1234 <162861691+import1234@users.noreply.github.com>
2024-11-27 20:13:59 +0900

    [Gold V] Title: 크리보드, Time: 96 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/11058. 크리보드/크리보드.py
+++ 백준/Gold/11058. 크리보드/크리보드.py
@@ -1,10 +1,6 @@
 n=int(input())
 dp=[*range(n+1)]
-for x in range(n):
-    if x-3>=0:
-        dp[x]=max(dp[x],dp[x-3]*2)
-        i=1
-        while x+i<=n:
-            dp[x+i]=max(dp[x+i],dp[x-3]*(2+i))
-            i+=1
+for x in range(3,n):
+    for y in range(n-x+1):
+        dp[x+y]=max(dp[x+y],dp[x-3]*(2+y))
 print(dp[-1])
