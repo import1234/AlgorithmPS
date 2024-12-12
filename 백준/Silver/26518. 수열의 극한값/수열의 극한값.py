bc95c71425b5262b19405051dd6102dbb778507b
import1234 <162861691+import1234@users.noreply.github.com>
2024-12-12 16:52:32 +0900

    [Silver II] Title: 수열의 극한값, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/26518. 수열의 극한값/수열의 극한값.py
+++ 백준/Silver/26518. 수열의 극한값/수열의 극한값.py
@@ -1,2 +1,5 @@
-b,c,_,__ = map(int,input().split())
-print((b+(b**2+4*c)**.5)/2)
+b,c,a1,a2=map(int, input().split())
+l=[0,a1,a2]
+for x in range(100):
+    l.append(b*l[-1]+c*l[-2])
+print(l[102]/l[101])
