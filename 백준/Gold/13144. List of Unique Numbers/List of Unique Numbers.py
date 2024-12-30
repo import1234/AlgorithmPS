8dca05f339496ba3ad5b7aa0f481dc1e66d0a2a7
import1234 <162861691+import1234@users.noreply.github.com>
2024-12-30 19:19:40 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 124 ms, Memory: 137552 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.py
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.py
@@ -1,13 +1,11 @@
-n=int(input())
-l=[*map(int,input().split())]
+n,*l=map(int,open(0).read().split())
 d={}
 c=t=m=0
 for x in range(n):
     if l[x] in d and d[l[x]]>=m:
-        t=x-d[l[x]]
         m=d[l[x]]
-    else:
-        t+=1
+        t=x-m
+    else:t+=1
     c+=t
     d[l[x]]=x
 print(c)
