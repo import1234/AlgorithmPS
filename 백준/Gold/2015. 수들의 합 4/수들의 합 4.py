eebf55140240670b9d7cff2536670d4d5f44d768
import1234 <162861691+import1234@users.noreply.github.com>
2024-12-31 09:45:01 +0900

    [Gold IV] Title: 수들의 합 4, Time: 132 ms, Memory: 138516 KB -BaekjoonHub

--- 백준/Gold/2015. 수들의 합 4/수들의 합 4.py
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.py
@@ -1,15 +1,10 @@
 n,k=map(int,input().split())
 l=[*map(int,input().split())]
-
-c=t=0
+c=t=u=0
 d={}
+for x in l:d[u]=d.get(u:=u+x,0)+1
 for x in l:
-    t+=x
-    d[t]=d.get(t,0)+1
-t=0
-for x in l:
-    t+=x
     c+=d.get(k,0)
-    if d.get(t):d[t]-=1
+    if d.get(t:=t+x):d[t]-=1
     k+=x
 print(c)
