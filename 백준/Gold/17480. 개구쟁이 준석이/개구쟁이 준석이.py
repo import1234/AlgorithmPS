a4d0a73c94ba9393567149ca155206eda8aa571e
import1234 <162861691+import1234@users.noreply.github.com>
2025-01-05 20:03:12 +0900

    [Gold III] Title: 개구쟁이 준석이, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Gold/17480. 개구쟁이 준석이/개구쟁이 준석이.py
@@ -0,0 +1,37 @@
+n=int(input())
+l=input().split()
+d={}
+for x in range(n):d[l[2*x]]=int(l[2*x+1])
+l=[]
+s=input()
+n=len(s)
+for x in range(n):
+    for y in range(x+1,n+1):
+        t={}
+        for z in s[x:y]:
+            t[z]=t.get(z,0)+1
+        if d==t:l.append(s[x:y])
+
+def f(x):
+    n=len(x)
+    if n==1:return [x]
+    s=set()
+    a,b=x[:n//2],x[n//2:]
+    for y in f(b):
+        s.add(a[::-1]+y)
+    for y in f(a):
+        s.add(y+b[::-1])
+    if n%2==1:
+        a,b=x[:n//2+1],x[n//2+1:]
+        for y in f(b):
+            s.add(a[::-1]+y)
+        for y in f(a):
+            s.add(y+b[::-1])
+    return s
+        
+
+s=set()
+for x in l:
+    for y in f(x):
+        s.add(y)
+print(len(s))
