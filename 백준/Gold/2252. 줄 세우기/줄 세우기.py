dde897b93c89ebcfdee346f389b806cf650bb0bd
import1234 <162861691+import1234@users.noreply.github.com>
2025-01-09 03:57:29 +0900

    [Gold III] Title: 줄 세우기, Time: 236 ms, Memory: 134716 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -3,30 +3,20 @@ f=lambda:map(int,input().split())
 n,m=f()
 ind=dd(int)
 d=dd(list)
-l=[]
 for x in' '*m:
     a,b=f()
     d[a].append(b)
     ind[b]+=1
 
-t=set(range(1,n+1))
-for x in ind:t.remove(x)
+t=set(range(1,n+1))-set(ind.keys())
 
 ans=[]
-while q:=t.copy():
+while q:=t:
+    t=set()
     for x in q:
         ans.append(x)
-        t.remove(x)
         for y in d[x]:
             ind[y]-=1
             if ind[y]==0:t.add(y)
 
 print(*ans)
-
-# #검산용
-# d={}
-# for x in range(len(ans)):d[ans[x]]=x
-# for x,y in l:
-#     if d[x]>d[y]:
-#         print('no')
-#         break
