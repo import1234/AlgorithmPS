eb6214199b7ef19fefce7bd0e6827a61b51df06f
import1234 <162861691+import1234@users.noreply.github.com>
2025-01-10 19:55:07 +0900

    [Gold II] Title: Facebook, Time: 912 ms, Memory: 120056 KB -BaekjoonHub

--- 백준/Gold/20501. Facebook/Facebook.py
+++ 백준/Gold/20501. Facebook/Facebook.py
@@ -1,10 +1,6 @@
 import sys
 i=sys.stdin.readline
-n=int(i())
-l=[]
-for x in' '*n:l.append(int(i(),2))
-q=int(i())
-for x in' '*q:
+l=[int(i(),2)for x in' '*int(i())]
+for x in' '*int(i()):
     a,b=map(int,i().split())
-    a-=1;b-=1
-    print((l[a]&l[b]).bit_count())
+    print((l[a-1]&l[b-1]).bit_count())
