c54d155e5e2db299f819ee547934338a8bb022f8
import1234 <162861691+import1234@users.noreply.github.com>
2025-01-13 01:18:40 +0900

    [Gold V] Title: 용액, Time: 136 ms, Memory: 127132 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -1,7 +1,7 @@
 n=int(input())
 l=[*map(int,input().split())]
 
-ans=[]
+ans=[float('inf'),0,0]
 for x in range(n):
     s=0
     e=n-1
@@ -10,7 +10,7 @@ for x in range(n):
         m=(s+e)//2
         if l[m]>=t:e=m-1
         else:s=m+1
-    def f(a):
-        if a<n and a!=x:ans.append([abs(l[x]+l[a]),l[x],l[a]])
-    f(s);f(s+1);f(s-1)
-print(*sorted(ans)[0][1:])
+    for y in [s-1,s,s+1]:
+        if 0<=y<n and x!=y and ans[0]>abs(l[x]+l[y]):
+            ans=[abs(l[x]+l[y]),l[x],l[y]]
+print(*ans[1:])
