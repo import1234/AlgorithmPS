ebc606f07659159d012fcae73f8136ab73df2431
import1234 <162861691+import1234@users.noreply.github.com>
2025-02-19 21:10:33 +0900

    [Gold IV] Title: 알파벳, Time: 332 ms, Memory: 204596 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,11 +1,5 @@
 r,c=map(int,input().split())
-l=[[*map(lambda x:2**(ord(x)-65),input())]for x in' '*r]
+l=[[*map(lambda x:1<<(ord(x)-65),input())]for _ in' '*r]
 t,C={(0,0,l[0][0])},0
-while q:=t:
- t=set()
- for x,y,z in q:
-  for a,b in[(1,0),(-2,0),(1,1),(0,-2)]:
-   x+=a;y+=b
-   if r>x>=0and c>y>=0and l[x][y]&z==0:t.add((x,y,z|l[x][y]))
- C+=1
+while q:=t:t={(a,b,z|l[a][b])for x,y,z in q for a,b in((x+1,y),(x-1,y),(x,y+1),(x,y-1))if r>a>=0and c>b>=0and l[a][b]&z==0};C+=1
 print(C)
