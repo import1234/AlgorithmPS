76c6dc1b971e8fc47f27efdcec6c24fb9420b49e
import1234 <162861691+import1234@users.noreply.github.com>
2025-03-10 00:11:54 +0900

    [Gold III] Title: 세 친구, Time: 184 ms, Memory: 32412 KB, Score: 35 point -BaekjoonHub

--- 백준/Gold/10096. 세 친구/세 친구.py
+++ 백준/Gold/10096. 세 친구/세 친구.py
@@ -1,11 +1,25 @@
+def f(a):print(a);exit()
 n=int(input())
-if n%2==0:print('NOT POSSIBLE');exit()
+if n%2==0:f('NOT POSSIBLE')
 l=input()
+if len(set([*l]))==1:f(l[:n//2])
+
 ans=set()
 for x in range(n):
-    a=l[:x]+l[x+1:]
-    t=a[:n//2]
-    if t==a[n//2:]:ans.add(t)
-    if len(ans)>1:print('NOT UNIQUE');exit()
+    a=0
+    b=n//2
+    if x<=n//2:b+=1
+    t=[]
+    yes=1
+    for y in range(n//2):
+        if a==x:a+=1
+        if b==x:b+=1
+        if l[a]==l[b]:t.append(l[a])
+        else:yes=0;break
+        a+=1
+        b+=1
+    if yes:
+        ans.add(''.join(t))
+        if len(ans)>1:f('NOT UNIQUE')
 if ans:print(*ans)
-else:print('NOT POSSIBLE')
+else:f('NOT POSSIBLE')
