b80c42911f461c6f65ed955a017fcd0eeda5ed5d
import1234 <162861691+import1234@users.noreply.github.com>
2025-03-12 20:44:54 +0900

    [Gold V] Title: No title, Time: 180 ms, Memory: 13808 KB -BaekjoonHub

--- 백준/Gold/32763. No title/No title.py
+++ 백준/Gold/32763. No title/No title.py
@@ -1,15 +1,13 @@
 n=int(input())
 l=[0]*n
-a,b=[1],[]
-c=[a,b]
+a,b=[1],[];c=[a,b]
 t=0
 for x in range(n-1):
-    print(f"? {x+1} * {x+2}",flush=1)
-    if'-'==input():t^=1
+    print('?',x+1,'*',x+2)
+    t^=input()=='-'
     l[x+1]=t
     c[t]+=[x+2]
 if len(a)<2:a=b
-print(f"? {a[0]} + {a[1]}",flush=1)
-v=1
-if(input()=='-')^l[a[0]-1]:v=0
-print('! '+' '.join('+'if v^x else '-' for x in l),flush=1)
+print('?',a[0],'+',a[1])
+v=input()=='-'
+print('!',*['-'if v^x^l[a[0]-1]else'+'for x in l])
