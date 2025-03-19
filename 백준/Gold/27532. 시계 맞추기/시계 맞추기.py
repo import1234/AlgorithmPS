b76dc3c87f526d2c05ca30d6dbfc2c322980cd51
import1234 <162861691+import1234@users.noreply.github.com>
2025-03-19 22:03:59 +0900

    [Gold V] Title: 시계 맞추기, Time: 132 ms, Memory: 110888 KB -BaekjoonHub

--- 백준/Gold/27532. 시계 맞추기/시계 맞추기.py
+++ 백준/Gold/27532. 시계 맞추기/시계 맞추기.py
@@ -1,14 +1,3 @@
 n=int(input())
-l=[]
-for x in range(n):
-    a,b=map(int,input().split(':'))
-    l.append((a%12)*60+b)
-a=n
-for r in range(720):
-    b=set()
-    for x in range(n):
-        if (l[x]-r*x)%720 not in b:
-            b.add((l[x]-r*x)%720)
-            if len(b)>a:break
-    a=min(len(b),a)
-print(a)
+l=[int(a)%12*60+int(b)for _ in' '*n for a,b in[input().split(':')]]
+print(min(len({(l[x]-r*x)%720for x in range(n)})for r in range(720)))
