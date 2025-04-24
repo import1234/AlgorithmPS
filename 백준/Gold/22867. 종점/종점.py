9017d341b435fb9082c19bb20e7a87838259d257
import1234 <162861691+import1234@users.noreply.github.com>
2025-04-24 20:46:12 +0900

    [Gold V] Title: 종점, Time: 536 ms, Memory: 193916 KB -BaekjoonHub

--- 백준/Gold/22867. 종점/종점.py
+++ 백준/Gold/22867. 종점/종점.py
@@ -1,18 +1,14 @@
 def f(x):return int(x[:2])*3600+int(x[3:5])*60+float(x[6:])
-d1={}
-d2={}
+d1={};d2={}
 for x in range(int(input())):
     a,b=map(f,input().split())
     if a in d1:d1[a].append(x)
     else:d1[a]=[x]
     if b in d2:d2[b].append(x)
     else:d2[b]=[x]
-ans=0
-bus=set()
+ans=bus=0
 for t in sorted(set([*d1.keys()]+[*d2.keys()])):
-    if t in d2:
-        for x in d2[t]:bus.remove(x)
-    if t in d1:
-        for x in d1[t]:bus.add(x)
-    ans=max(ans,len(bus))
+    if t in d2:bus-=len(d2[t])
+    if t in d1:bus+=len(d1[t])
+    ans=max(ans,bus)
 print(ans)
