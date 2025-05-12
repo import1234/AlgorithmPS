b96e61276709770fc9cb2649845c03bf99ce9af0
import1234 <162861691+import1234@users.noreply.github.com>
2025-05-12 21:59:49 +0900

    [Platinum V] Title: 수열과 쿼리와 확률 1, Time: 788 ms, Memory: 146348 KB, Score: 50 point -BaekjoonHub

--- 백준/Platinum/33469. 수열과 쿼리와 확률 1/수열과 쿼리와 확률 1.py
+++ 백준/Platinum/33469. 수열과 쿼리와 확률 1/수열과 쿼리와 확률 1.py
@@ -1,10 +1,9 @@
-f=lambda:map(int,input().split())
-n,m=f()
-l=[*f()]
+n,m=map(int,input().split())
+input()
 if input()=='S':print(1);exit()
 mod=10**9+7
 inv=lambda x:pow(x,mod-2,mod)
 t=inv(2)
-for x in range(1,n+1):t*=x*inv(n)
+for x in range(1,n+1):t=(t*x*inv(n))%mod
 t+=(n+1)*inv(4)
 print(pow(t,m,mod))
